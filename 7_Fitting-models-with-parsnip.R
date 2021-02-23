ef963472bfc638ce2bcb37c0e187122358734f46
Jonathan Leslie <jonathan.leslie@pivigo.com>
2021-02-23 22:47:59 +0000

    Add Chapter 7 - fitting models with parsnip

---
+++ 7_Fitting-models-with-parsnip.R
@@ -0,0 +1,84 @@
+source("6_feature-engineering.R")
+
+
+# 1.  Create the model ----------------------------------------------------
+
+lm_model <- 
+  linear_reg() %>% 
+  set_engine("lm")
+
+lm_form_fit <- 
+  lm_model %>% 
+  # Recall that Sale_Price has been pre-logged
+  fit(Sale_Price ~ Longitude + Latitude, data = ames_train)
+
+lm_xy_fit <- 
+  lm_model %>% 
+  fit_xy(
+    x = ames_train %>% select(Longitude, Latitude),
+    y = ames_train %>% pull(Sale_Price)
+  )
+
+lm_form_fit
+lm_xy_fit
+
+
+rand_forest(trees = 1000, min_n = 5) %>% 
+  set_engine("ranger") %>% 
+  set_mode("regression") %>% 
+  translate()
+
+
+# 7.2 USE THE MODEL RESULTS -----------------------------------------------
+
+lm_form_fit %>% pluck("fit")
+lm_xy_fit %>% pluck("fit")
+
+
+model_res <- 
+  lm_form_fit %>% 
+  pluck("fit") %>% 
+  summary()
+
+param_est <- coef(model_res)
+class(param_est)
+
+param_est
+
+tidy(lm_form_fit)
+
+
+# 7.3 MAKE PREDICTIONS ----------------------------------------------------
+
+ames_test_small <- ames_test %>% slice(1:5)
+predict(lm_form_fit, new_data = ames_test_small)
+
+ames_test_small %>% 
+  select(Sale_Price) %>% 
+  bind_cols(predict(lm_form_fit, ames_test_small)) %>% 
+  # Add 95% prediction intervals to the results:
+  bind_cols(predict(lm_form_fit, ames_test_small, type = "pred_int")) 
+
+
+
+# 7.5 Chapter summary -----------------------------------------------------
+
+library(tidymodels)
+data(ames)
+ames <- mutate(ames, Sale_Price = log10(Sale_Price))
+
+set.seed(123)
+ames_split <- initial_split(ames, prob = 0.80, strata = Sale_Price)
+ames_train <- training(ames_split)
+ames_test  <-  testing(ames_split)
+
+ames_rec <- 
+  recipe(Sale_Price ~ Neighborhood + Gr_Liv_Area + Year_Built + Bldg_Type + 
+           Latitude + Longitude, data = ames_train) %>%
+  step_log(Gr_Liv_Area, base = 10) %>% 
+  step_other(Neighborhood, threshold = 0.01) %>% 
+  step_dummy(all_nominal()) %>% 
+  step_interact( ~ Gr_Liv_Area:starts_with("Bldg_Type_") ) %>% 
+  step_ns(Latitude, Longitude, deg_free = 20)
+
+lm_model <- linear_reg() %>% set_engine("lm")
